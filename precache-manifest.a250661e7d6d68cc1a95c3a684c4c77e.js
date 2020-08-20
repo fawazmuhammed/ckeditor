f8964ceb02f9803406d0709ac1d984f58a15222b
BLRPC-118 <fawaz@resemblesystems.com>
2020-08-20 14:55:58 +0530

    Updates

---
+++ precache-manifest.a250661e7d6d68cc1a95c3a684c4c77e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad9026b1f98aa8925aa91e7d9e9df28",
+    "url": "/ckeditor/index.html"
+  },
+  {
+    "revision": "08b2a390553d10c10b97",
+    "url": "/ckeditor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "895bd9a82ea052eab252",
+    "url": "/ckeditor/static/js/2.558c2aa7.chunk.js"
+  },
+  {
+    "revision": "50b980594887393756966bde0f739078",
+    "url": "/ckeditor/static/js/2.558c2aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b2a390553d10c10b97",
+    "url": "/ckeditor/static/js/main.cbdfe701.chunk.js"
+  },
+  {
+    "revision": "7b6e5bf50846741baee2",
+    "url": "/ckeditor/static/js/runtime-main.e630c03e.js"
+  }
+]);
